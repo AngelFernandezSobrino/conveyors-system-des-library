ea993e82c2af16ec197584d4c4837f8fbd3d7f0f
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2022-03-24 11:02:35 +0100

    Topology testing

--- simulator/core.py
+++ simulator/core.py
@@ -1,8 +1,46 @@
-
 from timed_events_manager import TimedEventsManager
 
-events_manager = TimedEventsManager()
+from stopper import StopperInfo, Stopper
 
-for i in range(0, 100):
 
-    events_manager.run
+class Core:
+
+    def __init__(self, topology: list[StopperInfo]) -> None:
+
+        self.systemData = []
+        self.events_manager = TimedEventsManager()
+        self.topology = topology
+
+        for i, stopper in enumerate(topology):
+            self.systemData.append(
+                Stopper(i, self.topology, self.systemData,
+                        self.events_manager))
+
+
+if __name__ == '__main__':
+
+    topology = [{
+        'destiny': [1],
+        'steps': [8],
+        'behaviour': [1],
+        'rest_steps': [1]
+    }, {
+        'destiny': [2],
+        'steps': [8],
+        'behaviour': [1],
+        'rest_steps': [1]
+    }, {
+        'destiny': [3],
+        'steps': [8],
+        'behaviour': [1],
+        'rest_steps': [1]
+    }, {
+        'destiny': [0],
+        'steps': [8],
+        'behaviour': [1],
+        'rest_steps': [1]
+    }]
+
+    core = Core(topology)
+
+    print(core.systemData)
