53a770d8f89afd7e4d5136fb5470c2aa0a7f2291
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2022-05-10 23:52:30 +0200

    Add multiparameter controller function
    Change to typed dictionary

--- src/simulations/controller_example.py
+++ src/simulations/controller_example.py
@@ -1,34 +1,35 @@
 
-from simulator import ControllerBase
+from simulator import BehaviourController
+import simulator.behaviour_controller
 from simulator.objects import Stopper, Tray
 
 
-class ControllerExample(ControllerBase):
+class BehaviourControllerExample(BehaviourController):
     def __init__(self, system_description: dict):
         super().__init__(system_description)
 
-        self.external = {
+        self.external_functions = {
             0: external_0
         }
 
-        self.check_request_stopper = {
-            '1': check_request_stopper_1,
-            '2': check_request_stopper_2
+        self.check_request_functions = {
+            '1': check_request_1,
+            '2': check_request_2
         }
 
 
-def check_request_stopper_1(simulation_data: dict[str, Stopper]):
-    if simulation_data['1'].request_time < 10:
-        simulation_data['1'].lock('2')
+def check_request_1(data: simulator.behaviour_controller.CheckRequestData):
+    if data['events_register'].step - data['simulation']['1'].request_time < 10:
+        data['simulation']['1'].lock('2')
     else:
-        simulation_data['1'].unlock('2')
+        data['simulation']['1'].unlock('2')
 
 
-def check_request_stopper_2(simulation_data: dict[str, Stopper]):
-    if simulation_data['2'].request_time < 10:
-        simulation_data['2'].lock('3')
+def check_request_2(data: simulator.behaviour_controller.CheckRequestData):
+    if data['events_register'].step - data['simulation']['2'].request_time < 10:
+        data['simulation']['2'].lock('3')
     else:
-        simulation_data['2'].unlock('3')
+        data['simulation']['2'].unlock('3')
 
 
 def external_0(args):
