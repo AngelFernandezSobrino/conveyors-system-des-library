6b8b8ef7060c7d02eb701d55fef6c05014dc49a3
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2022-05-10 11:31:17 +0200

    Add time adquisition initialization and close

--- src/simulator/core.py
+++ src/simulator/core.py
@@ -9,6 +9,7 @@ from simulator.objects.stopper import Stopper
 
 if TYPE_CHECKING:
     import simulator.objects.system
+    import simulator.results_controller.ResultsController
 
 
 class SimulationConfig(TypedDict):
@@ -19,7 +20,7 @@ class SimulationConfig(TypedDict):
 
 class Core:
 
-    def __init__(self, system_description: simulator.objects.system.SystemDescription, behaviour_controller, results_controller) -> None:
+    def __init__(self, system_description: simulator.objects.system.SystemDescription, behaviour_controller, results_controller: simulator.results_controller.ResultsController) -> None:
 
         self.simulation_config = {'real_time_mode': False, 'real_time_step': 0, 'steps': 0}
 
@@ -37,7 +38,7 @@ class Core:
         for stopper_id, stopper_description in system_description.items():
             self.simulation_data[stopper_id] = Stopper(stopper_id, system_description, self.simulation_data,
                                                        self.events_manager, behaviour_controller, results_controller, False)
-        print(behaviour_controller.external)
+
         for step, external_function in behaviour_controller.external.items():
             self.events_manager.add(external_function, {'simulation_data': self.simulation_data}, step)
 
@@ -74,19 +75,22 @@ class Core:
             self.sim_thread()
 
     def sim_thead_real_time(self):
+        self.results_controller.update_all_times(self.simulation_data, self.events_manager.step)
         start_time = time.time()
         while self.run_flag and (self.simulation_config['steps'] == 0 or self.events_manager.step < self.simulation_config['steps']):
             self.events_manager.run()
             time.sleep(self.simulation_config['real_time_step'] - ((time.time() - start_time) % self.simulation_config['real_time_step']))
+        self.results_controller.update_all_times(self.simulation_data, self.events_manager.step)
 
     def sim_thread(self):
+        self.results_controller.update_all_times(self.simulation_data, self.events_manager.step)
         while self.run_flag and self.events_manager.step < self.simulation_config['steps']:
-            print(self.events_manager.step)
             self.events_manager.run()
+        self.results_controller.update_all_times(self.simulation_data, self.events_manager.step)
 
 
 if __name__ == '__main__':
-    from src.simulator.helpers.test_utils import system_description_example
+    from simulator.helpers.test_utils import system_description_example
 
     core = Core(system_description_example)
     core.set_config({'real_time_mode': False, 'real_time_step': 0, 'steps': 10})
