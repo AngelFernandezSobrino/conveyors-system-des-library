e5821629902672a24624d04f455392a5a9627fa6
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2023-04-18 10:20:11 +0200

    Clean states class, improve naming

--- desym/objects/stopper/states.py
+++ desym/objects/stopper/states.py
@@ -8,23 +8,21 @@ if TYPE_CHECKING:
 
 DestinyId = str
 
+# Stopper states class, implement the stopper state machine
+## States:
+# Available: The stopper is available to receive trays
+# Reserved: The stopper is reserved to receive a tray
+# Request: The stopper has a tray and is waiting for a destiny to be available
+# Move: The stopper is moving a tray to a destiny
+
 
 class State:
     def __init__(self, core: core.Stopper) -> None:
         self.c = core
 
-        # Available for tray to arrive
         self.available = True
-
-        # Stopper is reserved for a tray to arrive
         self.reserved = False
-        # When another stopper send a tray to this stopper, an input event is sent to this stopper to reserve it
-        # When the tray arrives the stopper goes to the request state
-
-        # Tray is in the stopper and waiting for the next step
         self.request = False
-
-        # Tray is moving to the next stopper in destiny
         self.move = {v: False for v in self.c.stopper_description["destiny"]}
 
         # The path is locked by the behaviour controller
@@ -35,25 +33,7 @@ class State:
             for destiny in self.c.stopper_description["destiny"]
         }
 
-        self.destiny_not_available = {
-            destiny_id: False for destiny_id in self.c.stopper_description["destiny"]
-        }
-
-        # Destinies can be available or not, depending on the destiny stopper state.
-
-    #     self.destiny_not_available_v2: dict[DestinyId, dict[core.StopperId, bool]] = {
-    #         destiny_id: {destiny_id: False} for destiny_id in self.c.stopper_description["destiny"]
-    #     }
-
-    # def post_init(self):
-    #     for destiny_id, destiny_stops in self.destiny_not_available_v2.items():
-    #         for input_id in self.c.simulation.stoppers[
-    #             destiny_id
-    #         ].input_stoppers_ids:
-    #             if input_id != self.c.stopper_id:
-    #                 destiny_stops[input_id] = False
-
-    def go_move(self, destiny: core.StopperId):
+    def go_move(self, destiny: core.StopperId) -> None:
         if not self.c.simulation.stoppers[destiny].its_available():
             raise Exception(
                 f"Destiny not available in the destiny stopper {destiny} for the stopper {self.c.stopper_id}"
@@ -65,39 +45,40 @@ class State:
         self.c.input_tray = None
         self.c.events_manager.push(
             Event(self.end_move, tuple(), {"destiny": destiny}),
-            self.c.move_steps[destiny],
+            self.c.behaviorInfo.move_steps[destiny],
         )
 
-        if self.c.default_stopped == 1:
+        if self.c.behaviorInfo.default_stopped == 1:
             self.management_stop[destiny] = True
 
-        if self.c.move_behaviour[destiny] == 1:
+        if self.c.behaviorInfo.move_behaviour[destiny] == 1:
             self.c.events_manager.push(
-                self.go_rest, {}, self.c.return_available_steps[destiny]
+                Event(self.go_available, tuple(), {}),
+                self.c.behaviorInfo.return_available_steps[destiny]
             )
-        if self.c.move_behaviour[destiny] == 0:
-            self.go_rest()
+        if self.c.behaviorInfo.move_behaviour[destiny] == 0:
+            self.go_available()
         self.c._state_change()
 
-    def end_move(self, destiny: str):
+    def end_move(self, destiny: str) -> None:
         self.move[destiny] = False
         self.c.output_events.tray_send(destiny)
-        if self.c.move_behaviour == 3:
-            self.go_rest()
+        if self.c.behaviorInfo.move_behaviour == 3:
+            self.go_available()
         self.c._state_change()
 
-    def go_rest(self):
+    def go_available(self) -> None:
         self.available = True
         self.c._process_return_rest()
         self.c.output_events.available_origins()
         self.c._state_change()
 
-    def go_reserved(self):
+    def go_reserved(self) -> None:
         self.available = False
         self.reserved = True
         self.c._state_change()
 
-    def go_request(self):
+    def go_request(self) -> None:
         self.reserved = False
         self.request = True
         self.c._check_request()
