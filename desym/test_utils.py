596077f33aa47ac1f5fea1321f665b8c9aab0b80
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2024-01-26 08:10:09 +0100

    Style test utils

--- desym/test_utils.py
+++ desym/test_utils.py
@@ -1,36 +1,36 @@
 from desym.objects.stopper.core import StopperDescription
 
 system_description_example: dict[str, StopperDescription] = {
-    '0': {
-        'destiny': ['1'],
-        'steps': [8],
-        'move_behaviour': ['fast'],
-        'rest_steps': [1],
-        'default_locked': False,
-        'priority': 0
+    "0": {
+        "destiny": ["1"],
+        "steps": [8],
+        "move_behaviour": ["fast"],
+        "rest_steps": [1],
+        "default_locked": False,
+        "priority": 0,
     },
-    '1': {
-        'destiny': ['2'],
-        'steps': [8],
-        'move_behaviour': ['fast'],
-        'rest_steps': [1],
-        'default_locked': False,
-        'priority': 0
+    "1": {
+        "destiny": ["2"],
+        "steps": [8],
+        "move_behaviour": ["fast"],
+        "rest_steps": [1],
+        "default_locked": False,
+        "priority": 0,
     },
-    '2': {
-        'destiny': ['3'],
-        'steps': [8],
-        'move_behaviour': ['fast'],
-        'rest_steps': [1],
-        'default_locked': False,
-        'priority': 0
+    "2": {
+        "destiny": ["3"],
+        "steps": [8],
+        "move_behaviour": ["fast"],
+        "rest_steps": [1],
+        "default_locked": False,
+        "priority": 0,
     },
-    '3': {
-        'destiny': ['0'],
-        'steps': [8],
-        'move_behaviour': ['fast'],
-        'rest_steps': [1],
-        'default_locked': False,
-        'priority': 0
-    }
+    "3": {
+        "destiny": ["0"],
+        "steps": [8],
+        "move_behaviour": ["fast"],
+        "rest_steps": [1],
+        "default_locked": False,
+        "priority": 0,
+    },
 }
