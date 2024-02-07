05a24321c8a5b8cd136cfba36e024beda06d71fe
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2024-02-07 22:40:01 +0100

    Simulator factory functions

---
+++ sim/factory.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import time
+import os
+
+from desim.events_manager import CustomEventListener
+
+import logging
+
+from sim.item import Product
+import sim.settings as settings
+
+import sim.data_storage as data_storage
+
+import desim.core
+
+if TYPE_CHECKING:
+    import desim.config_parser
+
+import sim.controller as custom_behaviour
+
+logger = logging.getLogger("simul")
+logFormatter = logging.Formatter("\N{ESC}[0m{name: <30s} - {message}", style="{")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+def get_simulator_config():
+
+    config_path = os.path.dirname(os.path.abspath(__file__)) + "/config.xlsx"
+    config_parser = desim.config_parser.ConfigParser(config_path)
+    config_parser.parse("config_parser")
+
+    return config_parser
+def create_simulator(config_parser: desim.config_parser.ConfigParser, verbose: bool = False):
+
+    if verbose:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    logger.setLevel(logging_level)
+
+    sim_core = desim.core.Simulation[Product](config_parser.config, debug=verbose)
+
+    behavior = custom_behaviour.SimulationController(
+        sim_core, settings.MAX_CONTAINERS_AMMOUNT
+    )
+
+    sim_core.register_external_events(
+        behavior.external_functions,
+        # step_callback,
+    )
+
+    for data_storage_step in range(0, settings.STEPS, 100):
+        sim_core.timed_events_manager.add(
+            CustomEventListener(
+                data_storage.save_actual_results, (), {"controller": behavior}
+            ),
+            data_storage_step,
+        )
+
+    return sim_core, behavior
+
+
+    behavior.results_time.update_all_times()
+
+
+    ## Print simulation results
+
+    logger.info(f"Simulation spent time: {time.time() - start}")
+    logger.info(f"Simulation duration: {settings.STEPS*settings.STEP_TO_TIME / 3600} hours")
+    logger.info("Production results:")
+    logger.info(
+        f"Product {item.ProductTypeReferences.product_1.name}: {behavior.results_production.counters[item.ProductTypeReferences.product_1]}"
+    )
+    logger.info(
+        f"Product {item.ProductTypeReferences.product_2.name}: {behavior.results_production.counters[item.ProductTypeReferences.product_2]}"
+    )
+    logger.info(
+        f"Product {item.ProductTypeReferences.product_3.name}: {behavior.results_production.counters[item.ProductTypeReferences.product_3]}"
+    )
+
+    data_storage.save_data_to_file()
